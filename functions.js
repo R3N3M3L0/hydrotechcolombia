f4e80dc3aca13091679f10843b72c0c5c608f343
R3N3M3L0 <95933679+R3N3M3L0@users.noreply.github.com>
2021-12-26 22:04:27 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,12 @@
 function desplegarMenu() 
 {
     document.getElementById("menuMovil").style.display="block";
+    document.getElementById("navcel").style.display="none";
+    document.getElementById("xnavcel").style.display="block";
+}
+function qmenu() 
+{
+    document.getElementById("menuMovil").style.display="none";
+    document.getElementById("navcel").style.display="block";
+    document.getElementById("xnavcel").style.display="none";
 }
